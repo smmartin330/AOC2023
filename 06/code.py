9171e94754740dbcc7148aa0d0a3e1c11451241e
Stephen Martin <smm330@icloud.com>
2023-12-06 09:19:02 -0500

    day 6 was a reprieve

---
+++ 06/code.py
@@ -0,0 +1,160 @@
+import argparse
+from time import time
+import json
+import math
+
+DAY = 6
+
+PUZZLE_TEXT = '''
+--- Day 6: Wait For It ---
+
+The ferry quickly brings you across Island Island. After asking around, you discover that there is indeed normally a large pile of sand somewhere near here, but you don't see anything besides lots of water and the small island where the ferry has docked.
+
+As you try to figure out what to do next, you notice a poster on a wall near the ferry dock. "Boat races! Open to the public! Grand prize is an all-expenses-paid trip to Desert Island!" That must be where the sand comes from! Best of all, the boat races are starting in just a few minutes.
+
+You manage to sign up as a competitor in the boat races just in time. The organizer explains that it's not really a traditional race - instead, you will get a fixed amount of time during which your boat has to travel as far as it can, and you win if your boat goes the farthest.
+
+As part of signing up, you get a sheet of paper (your puzzle input) that lists the time allowed for each race and also the best distance ever recorded in that race. To guarantee you win the grand prize, you need to make sure you go farther in each race than the current record holder.
+
+The organizer brings you over to the area where the boat races are held. The boats are much smaller than you expected - they're actually toy boats, each with a big button on top. Holding down the button charges the boat, and releasing the button allows the boat to move. Boats move faster if their button was held longer, but time spent holding the button counts against the total race time. You can only hold the button at the start of the race, and boats don't move until the button is released.
+
+For example:
+
+Time:      7  15   30
+Distance:  9  40  200
+This document describes three races:
+
+The first race lasts 7 milliseconds. The record distance in this race is 9 millimeters.
+The second race lasts 15 milliseconds. The record distance in this race is 40 millimeters.
+The third race lasts 30 milliseconds. The record distance in this race is 200 millimeters.
+Your toy boat has a starting speed of zero millimeters per millisecond. For each whole millisecond you spend at the beginning of the race holding down the button, the boat's speed increases by one millimeter per millisecond.
+
+So, because the first race lasts 7 milliseconds, you only have a few options:
+
+Don't hold the button at all (that is, hold it for 0 milliseconds) at the start of the race. The boat won't move; it will have traveled 0 millimeters by the end of the race.
+Hold the button for 1 millisecond at the start of the race. Then, the boat will travel at a speed of 1 millimeter per millisecond for 6 milliseconds, reaching a total distance traveled of 6 millimeters.
+Hold the button for 2 milliseconds, giving the boat a speed of 2 millimeters per millisecond. It will then get 5 milliseconds to move, reaching a total distance of 10 millimeters.
+Hold the button for 3 milliseconds. After its remaining 4 milliseconds of travel time, the boat will have gone 12 millimeters.
+Hold the button for 4 milliseconds. After its remaining 3 milliseconds of travel time, the boat will have gone 12 millimeters.
+Hold the button for 5 milliseconds, causing the boat to travel a total of 10 millimeters.
+Hold the button for 6 milliseconds, causing the boat to travel a total of 6 millimeters.
+Hold the button for 7 milliseconds. That's the entire duration of the race. You never let go of the button. The boat can't move until you let go of the button. Please make sure you let go of the button so the boat gets to move. 0 millimeters.
+Since the current record for this race is 9 millimeters, there are actually 4 different ways you could win: you could hold the button for 2, 3, 4, or 5 milliseconds at the start of the race.
+
+In the second race, you could hold the button for at least 4 milliseconds and at most 11 milliseconds and beat the record, a total of 8 different ways to win.
+
+In the third race, you could hold the button for at least 11 milliseconds and no more than 19 milliseconds and still beat the record, a total of 9 ways you could win.
+
+To see how much margin of error you have, determine the number of ways you can beat the record in each race; in this example, if you multiply these values together, you get 288 (4 * 8 * 9).
+
+Determine the number of ways you could beat the record in each race. What do you get if you multiply these numbers together?
+'''
+
+SAMPLE_INPUT = '''
+Time:      7  15   30
+Distance:  9  40  200
+'''
+
+PUZZLE_INPUT = '''
+Time:        51     69     98     78
+Distance:   377   1171   1224   1505
+'''
+
+P1_SAMPLE_SOLUTION = 288
+
+P2_SAMPLE_SOLUTION = 71503
+
+def elapsed_time(start_time):
+    return f"{round(time() - start_time, 8)}s\n"
+
+class Puzzle():
+    def __init__(self,input_text):
+        self.input_text = input_text
+        self.input_list = input_text.strip().split('\n')
+
+        self.times = self.input_list[0][11:].split()
+        self.distances = self.input_list[1][11:].split()
+        self.big_time = ''
+        self.big_distance = ''
+        for time in self.times:
+            self.big_time += time
+        for distance in self.distances:
+            self.big_distance += distance
+                
+    def p1(self):
+        self.p1_solution = 1
+        for race in range(0,len(self.times)):
+            winners = 0
+            winning_distance = int(self.distances[race])
+            total_time = int(self.times[race])
+            for time_held in range(0,total_time+1):
+                if time_held * (total_time - time_held) > winning_distance:
+                    winners +=1
+            self.p1_solution *= winners
+        
+        return True
+
+    def p2(self):
+        self.p2_solution = 0
+        self.big_time = int(self.big_time)
+        self.big_distance = int(self.big_distance)
+        for time_held in range(0,int(self.big_time)+1):
+            if time_held * (self.big_time - time_held) > self.big_distance:
+                self.p2_solution +=1
+        return True
+
+def main():
+    parser = argparse.ArgumentParser(description=f'AOC2023 Puzzle Day { DAY }')
+    parser.add_argument("-p", "--showpuzzle", help="Display Puzzle Text", action='store_true')
+    parser.add_argument("-s", "--showsample", help="Display Sample Input", action='store_true')
+    args = parser.parse_args()
+    
+    if args.showpuzzle:
+        print(f"###############\nAOC 2023 DAY {DAY} PUZZLE TEXT\n###############")
+        print(PUZZLE_TEXT)
+    
+    if args.showsample:
+        print(f"###############\nAOC 2023 DAY {DAY} SAMPLE INPUT\n###############")
+        print(SAMPLE_INPUT.strip())
+        print(f"\n###############\nAOC 2023 DAY {DAY} P1 SAMPLE SOLUTION\n###############")
+        print(P1_SAMPLE_SOLUTION)
+        print(f"\n###############\nAOC 2023 DAY {DAY} P2 SAMPLE SOLUTION\n###############")
+        print(P2_SAMPLE_SOLUTION)
+    
+
+    if P1_SAMPLE_SOLUTION:            
+        print("PART 1\nTesting Sample...\n")
+        start_time = time()
+        sample = Puzzle(input_text=SAMPLE_INPUT)
+        sample.p1()
+        if P1_SAMPLE_SOLUTION == sample.p1_solution:
+            print("Sample correct.")
+        else:
+            print(f"Sample failed; Expected {P1_SAMPLE_SOLUTION}, got {sample.p1_solution}")
+        print(f"Elapsed time {elapsed_time(start_time)}")
+        if PUZZLE_INPUT:
+            puzzle = Puzzle(input_text=PUZZLE_INPUT)
+            puzzle.p1()
+            print("Processing Input...\n")
+            start_time = time()
+            print(f'SOLUTION: {puzzle.p1_solution}')
+            print(f"Elapsed time {elapsed_time(start_time)}")
+        
+    if P2_SAMPLE_SOLUTION:
+        print("PART 2\nTesting Sample...\n")
+        start_time = time()
+        sample.p2()
+        if P2_SAMPLE_SOLUTION == sample.p2_solution:
+            print("Sample correct.")
+        else:
+            print(f"Sample failed; Expected {P2_SAMPLE_SOLUTION}, got {sample.p2_solution}")
+        print(f"Elapsed time {elapsed_time(start_time)}")
+        if PUZZLE_INPUT:
+            print("Processing Input...\n")
+            start_time = time()
+            puzzle.p2()
+            print(f'SOLUTION: {puzzle.p2_solution}')
+            print(f"Elapsed time {elapsed_time(start_time)}")
+    
+if __name__ == "__main__":
+    main()
